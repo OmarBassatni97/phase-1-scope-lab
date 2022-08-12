f28f7aa234866b75c2535cd5214d27dee67ac885
omarbassatni <omarbassatni@gmail.com>
2022-08-12 13:31:46 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// Write your solution in this filevar 
+var customerName = 'bob'
+const leastFavoriteCustomer = 'john'
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'maybe bob'
+}
